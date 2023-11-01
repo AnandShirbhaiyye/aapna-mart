19b9475df9b8a02764920559e03760779d5baa9e
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-11-01 19:01:08 +0530

    feat: added get products api integration

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,17 +1,43 @@
-import React, { useEffect } from 'react'
-import Navbar from '../../components/Navbar/Navbar'
+import React, { useEffect, useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import axios from "axios";
 
 function Home() {
+  const [products, setProducts] = useState([]);
 
-  useEffect(()=>{
+  const loadProducts = async () => {
+    try {
+      const response = await axios.get("/products");
+      console.log(response);
+      setProducts(response?.data?.data);
+    } catch (err) {
+      console.log(err);
+      alert("error loading products");
+    }
+  };
 
-  },[])
-  return ( 
-   <>
-   <Navbar/>
-   <h1 className='text-center'>Home</h1>
-   </>
-  )
+  useEffect(() => {
+    loadProducts();
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-center">products</h1>
+      {products?.map((product, index) => {
+        const { name, price, image, description } = product;
+        return (
+          <ProductCard
+            key={index}
+            name={name}
+            description={description}
+            price={price}
+            image={image}
+          />
+        );
+      })}
+    </>
+  );
 }
 
-export default Home
+export default Home;
